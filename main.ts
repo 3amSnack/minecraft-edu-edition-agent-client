36660d475f4bd2d7320e1ee618e0669ad36bc515
3amSnack <148512237+3amSnack@users.noreply.github.com>
2024-06-26 16:45:25 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,115 +2,182 @@ player.onChat(".w-lock", function () {
     player.execute(
     "gamerule doweathercycle false"
     )
-})
-player.onChat(".kill", function () {
-    mobs.kill(
-    mobs.target(LOCAL_PLAYER)
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Weather Cycle Disabled\"}]}"
     )
 })
 player.onChat(".cleareffects", function () {
     mobs.clearEffect(mobs.target(NEAREST_PLAYER))
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Successfully Cleared all Effects\"}]}"
+    )
 })
 player.onChat(".t-lock", function () {
     player.execute(
     "gamerule dodaylightcycle false"
     )
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Daylight Cycle Disabled\"}]}"
+    )
+})
+player.onChat(".water-br", function () {
+    player.execute(
+    "effect @s water_breathing 999999 0 true"
+    )
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Granted Water Breathing\"}]}"
+    )
 })
 player.onChat(".w-thunder", function () {
     gameplay.setWeather(THUNDER)
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Set Weather to Thunder\"}]}"
+    )
+})
+player.onChat(".speed", function () {
+    player.execute(
+    "effect @s speed 999999 2 true"
+    )
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Granted Speed\"}]}"
+    )
 })
 player.onChat(".d-easy", function () {
     gameplay.setDifficulty(EASY)
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Set Difficulty to 1 (Easy)\"}]}"
+    )
 })
 player.onChat(".t-dawn", function () {
     gameplay.timeSet(gameplay.time(DAWN))
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Set Time to Dawn\"}]}"
+    )
 })
 player.onChat(".gms", function () {
     player.execute(
     "gamemode s"
     )
-    gamemode = 2
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Set Gamemode to 0 (Survival)\"}]}"
+    )
+})
+player.onChat(".one-hit", function () {
+    player.execute(
+    "effect @s strength 999999 255 true"
+    )
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Granted Strength\"}]}"
+    )
 })
 player.onChat(".t-unlock", function () {
     player.execute(
     "gamerule dodaylightcycle true"
     )
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Daylight Cycle Enabled\"}]}"
+    )
 })
 player.onChat(".t-midnight", function () {
     gameplay.timeSet(gameplay.time(MIDNIGHT))
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Set Time to Midnight\"}]}"
+    )
 })
 player.onChat(".t-dusk", function () {
     gameplay.timeSet(gameplay.time(DUSK))
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Set Time to Dusk\"}]}"
+    )
 })
 player.onChat(".gma", function () {
     player.execute(
     "gamemode a"
     )
-    gamemode = 3
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Set Gamemode to 2 (Adventure)\"}]}"
+    )
+})
+player.onChat(".xp", function (xpamount) {
+    gameplay.xp(xpamount, mobs.target(LOCAL_PLAYER))
+    player.say("Gave Himself " + xpamount + " XP")
 })
 player.onChat(".w-rain", function () {
     gameplay.setWeather(RAIN)
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Set Weather to Rain\"}]}"
+    )
 })
 player.onChat(".w-unlock", function () {
     player.execute(
     "gamerule doweathercycle true"
     )
-})
-player.onChat(".instakill", function () {
     player.execute(
-    "effect @s strength 999999 255 true"
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Weather Cycle Enabled\"}]}"
     )
 })
 player.onChat(".fly", function () {
     player.execute(
     "ability @s mayfly true"
     )
-})
-player.onChat(".spawnpoint", function () {
     player.execute(
-    "spawnpoint @s ~~~"
-    )
-})
-player.onChat(".tp", function (x, y, z) {
-    mobs.teleportToPosition(
-    mobs.target(LOCAL_PLAYER),
-    pos(x, y, z)
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Set Flight to True\"}]}"
     )
 })
 player.onChat(".d-hard", function () {
     gameplay.setDifficulty(HARD)
-})
-player.onChat(".xp", function (amount) {
-    gameplay.xp(amount, mobs.target(LOCAL_PLAYER))
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Set Difficulty to 3 (Hard)\"}]}"
+    )
 })
 player.onChat(".instamine", function () {
     player.execute(
     "effect @s haste 999999 255 true"
     )
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Granted Haste\"}]}"
+    )
 })
 player.onChat(".t-midday", function () {
     gameplay.timeSet(gameplay.time(MIDDAY))
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Set Time to Midday\"}]}"
+    )
 })
 player.onChat(".d-normal", function () {
     gameplay.setDifficulty(NORMAL)
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Set Difficulty to 2 (Normal)\"}]}"
+    )
 })
 player.onChat(".d-peaceful", function () {
     gameplay.setDifficulty(PEACEFUL)
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Set Difficulty to 0 (Peaceful)\"}]}"
+    )
 })
 player.onChat(".stopfly", function () {
     player.execute(
     "ability @s mayfly false"
+    )
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Set Flight to False\"}]}"
     )
 })
 player.onChat(".gmc", function () {
     player.execute(
     "gamemode c"
     )
-    gamemode = 1
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Set Gamemode to 1 (Creative)\"}]}"
+    )
 })
 player.onChat(".godmode", function () {
     player.execute(
     "effect @s resistance 999999 255 true"
+    )
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Granted Resistance\"}]}"
     )
 })
 player.onChat(".give", function (item, amount) {
@@ -122,13 +189,10 @@ player.onChat(".give", function (item, amount) {
 })
 player.onChat(".help", function () {
     player.execute(
-    "tellraw @s {\"rawtext\":[{\"text\":\"--------------------------------------------------------------------------\"}]}"
-    )
-    player.execute(
     "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lAgent§r> .d-[difficulty]\"}]}"
     )
     player.execute(
-    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lAgent§r> .t-[time] (day, dawn, midday, dusk, night, midnight, lock, unlock)\"}]}"
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lAgent§r> .t-[time] (dawn, day, midday, dusk, night, midnight, lock, unlock)\"}]}"
     )
     player.execute(
     "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lAgent§r> .w-[weather] (clear, rain. thunder, lock, unlock)\"}]}"
@@ -137,10 +201,10 @@ player.onChat(".help", function () {
     "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lAgent§r> .give [item id] [amount]\"}]}"
     )
     player.execute(
-    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lAgent§r> .gmc\"}]}"
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lAgent§r> .gms\"}]}"
     )
     player.execute(
-    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lAgent§r> .gms\"}]}"
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lAgent§r> .gmc\"}]}"
     )
     player.execute(
     "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lAgent§r> .gma\"}]}"
@@ -152,7 +216,7 @@ player.onChat(".help", function () {
     "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lAgent§r> .stopfly\"}]}"
     )
     player.execute(
-    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lAgent§r> .kill\"}]}"
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lAgent§r> .suicide\"}]}"
     )
     player.execute(
     "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lAgent§r> .fullbright\"}]}"
@@ -161,7 +225,13 @@ player.onChat(".help", function () {
     "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lAgent§r> .instamine\"}]}"
     )
     player.execute(
-    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lAgent§r> .instakill\"}]}"
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lAgent§r> .one-hit\"}]}"
+    )
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lAgent§r> .water-br\"}]}"
+    )
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lAgent§r> .speed\"}]}"
     )
     player.execute(
     "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lAgent§r> .godmode\"}]}"
@@ -172,31 +242,39 @@ player.onChat(".help", function () {
     player.execute(
     "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lAgent§r> .xp [amount]\"}]}"
     )
-    player.execute(
-    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lAgent§r> .tp [x] [y] [z]\"}]}"
-    )
-    player.execute(
-    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lAgent§r> .spawnpoint\"}]}"
-    )
-    player.execute(
-    "tellraw @s {\"rawtext\":[{\"text\":\"--------------------------------------------------------------------------\"}]}"
-    )
 })
 player.onChat(".fullbright", function () {
     player.execute(
     "effect @s night_vision 999999 1 true"
     )
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Granted Night Vision\"}]}"
+    )
+})
+player.onChat(".suicide", function () {
+    mobs.kill(
+    mobs.target(LOCAL_PLAYER)
+    )
+    player.say("Killed Himself")
 })
 player.onChat(".t-night", function () {
     gameplay.timeSet(gameplay.time(NIGHT))
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Set Time to Night\"}]}"
+    )
 })
 player.onChat(".w-clear", function () {
     gameplay.setWeather(CLEAR)
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Set Weather to Clear\"}]}"
+    )
 })
 player.onChat(".t-day", function () {
     gameplay.timeSet(gameplay.time(DAY))
+    player.execute(
+    "tellraw @s {\"rawtext\":[{\"text\":\"<§8§lNotification§r> Set Time to Day\"}]}"
+    )
 })
-let gamemode = 0
 player.execute(
 "title @s title §8§lAgent Client V1"
 )
